c5efffba7e872501747b42e6247568a3a320e57d
Rob Howley <rob.howley@shopify.com>
2026-08-29 23:32:06 -0400

    refactor(pi-okf-search): narrow runtime search dependency

--- packages/pi-okf-search/extensions/okf-search/runtime.ts
+++ packages/pi-okf-search/extensions/okf-search/runtime.ts
@@ -14,15 +14,20 @@ type RuntimeContext = Pick<
   "cwd" | "isProjectTrusted"
 >;
 
+export type RuntimeSearchHandle = Pick<
+  OkfSearch,
+  "search" | "listTypes" | "listDegradedDocuments"
+>;
+
 interface Snapshot {
   readonly root: string;
-  readonly search: OkfSearch;
+  readonly search: RuntimeSearchHandle;
   readonly indexedAt: number;
 }
 
 interface RuntimeDependencies {
   readonly loadConfig?: typeof loadConfig;
-  readonly openOkf?: typeof openOkf;
+  readonly openOkf?: (root: string) => Promise<RuntimeSearchHandle>;
   readonly now?: () => number;
 }
 
